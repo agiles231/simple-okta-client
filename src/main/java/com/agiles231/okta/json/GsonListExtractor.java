9b1723f92882af92040b77d95343871ee035532d
U-CYSEMI\anng <anng@KYCC-WIN-ANNG.cysemi.com>
2019-11-06 10:34:00 -0500

    Return ArrayList to support add and remove

--- src/main/java/com/agiles231/okta/json/GsonListExtractor.java
+++ src/main/java/com/agiles231/okta/json/GsonListExtractor.java
@@ -1,6 +1,7 @@
 package com.agiles231.okta.json;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 
@@ -20,8 +21,8 @@ public class GsonListExtractor implements JsonCollectionExtractor {
 
     @Override
     public <T> Collection<T> extractCollection(String jsonContent, Class<T> clazz) {
-//        logger.debug("jsonContent: " + jsonContent);
-        return Arrays.asList(gson.fromJson(jsonContent, (Class<T[]>) Array.newInstance(clazz, 0).getClass()));
+        logger.debug("jsonContent: " + jsonContent);
+        return new ArrayList<>(Arrays.asList(gson.fromJson(jsonContent, (Class<T[]>) Array.newInstance(clazz, 0).getClass()))); // surround with arraylist so that add and remove are supported
     }
 
 }
